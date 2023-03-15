ea0af359a0a365650fba19346b0be5f1e73e37af
Rahul Kumar Bhikundia <91981344+RahulBhikundia@users.noreply.github.com>
2023-03-15 10:08:43 +0530

    Adding a program which explian how to use constructor in java

---
+++ constructor.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+
+class Greetings{
+    private String fname, lname;
+    Greetings(String first,String last){
+        fname = first;
+        lname = last;
+    }
+    public void greet(){
+        System.out.println("Hello "+fname+" "+lname+". Let's start Java Programming.");
+    }
+}
+
+
+public class constructor {
+    public static void main(String arg[]){
+        String fname, lname;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your First Name:");
+        fname = sc.nextLine();
+        System.out.print("Enter Your last Name:");
+        lname = sc.nextLine();
+        Greetings Obj = new Greetings(fname, lname);
+        Obj.greet(); 
+    }
+}
