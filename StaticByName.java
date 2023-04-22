f622636d345b18d4356f93b6078e39748b72b98b
Rahul Kumar Bhikundia <91981344+RahulBhikundia@users.noreply.github.com>
2023-04-22 21:20:11 +0530

    Adding java programs which describe how to declare static members of clas and how to use them.

---
+++ StaticByName.java
@@ -0,0 +1,19 @@
+// Demo to show how to use static members of class outside their class
+
+class StaticDemo
+{
+    static int a = 42;
+    static int b = 99;
+
+    static void callme()
+    {
+        System.out.println("a = "+a);
+    }
+}
+class StaticByName {
+    public static void main(String args[])
+    {
+        StaticDemo.callme();
+        System.out.println("b = "+StaticDemo.b);
+    }
+}
