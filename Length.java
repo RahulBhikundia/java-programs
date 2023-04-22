b2b5745305a2fb9b37e9ed6d88ee2973dbfc96bb
Rahul Kumar Bhikundia <91981344+RahulBhikundia@users.noreply.github.com>
2023-04-22 21:26:53 +0530

    Adding java program which explains lenth member of Array instance.

---
+++ Length.java
@@ -0,0 +1,13 @@
+class Length 
+{
+    public static void main(String args[])
+    {
+        int a1[] = new int[10];
+        int a2[] = {3, 4, 5, 6, -2, 34, 7, 9, 1};
+        int a3[] = {4, 3, 2, 1};
+
+        System.out.println("length of a1 : "+a1.length);
+        System.out.println("length of a2 : "+a2.length);
+        System.out.println("length of a3 : "+a3.length);
+    }    
+}
