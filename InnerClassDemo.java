879bcf22ffcd5db19d5c1b51ac673702264df381
Rahul Kumar Bhikundia <91981344+RahulBhikundia@users.noreply.github.com>
2023-04-23 08:23:39 +0530

    Java Program demonstrating Nested classes

---
+++ InnerClassDemo.java
@@ -0,0 +1,28 @@
+// Demonstrating an inner class
+class Outer
+{
+    int outer_x = 100;
+
+    void test()
+    {
+        Inner inner = new Inner(); 
+        inner.display();
+    }
+
+    // this is an inner class
+    class Inner
+    {
+        void display()
+        {
+            System.out.println("outer_x = "+outer_x);
+        }
+    }
+}
+class InnerClassDemo 
+{
+    public static void main(String args[])
+    {
+        Outer outer = new Outer();
+        outer.test();
+    }
+}
